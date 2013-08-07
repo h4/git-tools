7f86d3bd250af6545e8f533c068068f369dcc095
Mikhail Baranov <mikhail.baranov@gmail.com>
2013-08-07 12:19:10 +0400

    Заглушка для функции get_merged_branches()

--- git-clean-merged.sh
+++ git-clean-merged.sh
@@ -9,12 +9,17 @@ MERGED=""
 
 init() {
 	require_git_repo
+	get_merged_branches
 }
 
 require_git_repo() {
 	if ! git rev-parse --git-dir >/dev/null 2>&1; then
 		die "fatal: Not a git repository"
 	fi
+}
+
+get_merged_branches() {
+	echo "Branches already merged into branch ${MAIN_BRANCH}:" 
 }
 
 init
