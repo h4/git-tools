6ff8bd4ded50c66ba7ffb41bc1602c33afe01227
Mikhail Baranov <mikhail.baranov@gmail.com>
2013-08-07 14:54:24 +0400

    Добавил функцию проверки исключенных веток

--- git-clean-merged.sh
+++ git-clean-merged.sh
@@ -6,10 +6,12 @@ die() { warn "$@"; exit 1; }
 
 MAIN_BRANCH="develop"
 MERGED=""
+EXCLUDED="master develop"
 
 init() {
 	require_git_repo
 	get_merged_branches
+	delete_merged
 }
 
 require_git_repo() {
@@ -20,6 +22,25 @@ require_git_repo() {
 
 get_merged_branches() {
 	echo "Branches already merged into branch ${MAIN_BRANCH}:" 
+	MERGED=$( git branch --merged ${MAIN_BRANCH}  | cut -c 3-)
+	# TODO: добавить проверку на отсутсвие ключа -f
+	for branch in $MERGED
+		do echo "  $branch"
+	done
+}
+
+branch_is_excluded() {
+	current_brahch=$1
+	for excluded_branch in $EXCLUDED
+		do
+			if [ "${excluded_branch}" = "${current_brahch}" ]
+				then 
+				return 1
+			fi
+	done
+	return 0
+}
+
 }
 
 init
